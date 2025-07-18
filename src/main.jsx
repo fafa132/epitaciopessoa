7517f56f66b01ab9df213cf9491aec2c8292ecf6
fafa132 <46932923+fafa132@users.noreply.github.com>
2025-07-17 23:40:26 -0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <React.StrictMode>
+   <BrowserRouter basename="/epitaciopessoa/">
+     <Routes>
+       <Route path="/*" element={<App />} />
+     </Routes>
+   </BrowserRouter>
+ </React.StrictMode>
+);
